6f880b72f514b7ec52fda47913a2f138f8467ce4
Igor <montru@inbox.ru>
2022-11-07 00:39:08 +0100

    Added class Solution.cs

---
+++ SomeCoding/LC/Graphs/MinimumHeightTrees/MinimumHeightTrees/Solution.cs
@@ -0,0 +1,14 @@
+﻿namespace MinimumHeightTrees;
+
+public class Solution
+{
+    public IList<int> FindMinHeightTrees(int n, int[][] edges)
+    {
+        GraphStructure graph = GraphStructure.CreateFrom(n, edges);
+
+        GraphProcessor graphProcessor = new GraphProcessor(graph);
+
+        var roots = graphProcessor.FindRoots();
+        return roots;
+    }
+}
