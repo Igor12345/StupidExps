1326462b95d88c84552120934229e05bd4f6a369
Igor <montru@inbox.ru>
2022-11-12 16:17:23 +0100

    First steps

---
+++ SomeCoding/LC/MinimumJumpsToReachHome1654/Task/Task.cs
@@ -0,0 +1,31 @@
+﻿namespace Task;
+
+public class Task
+{
+    public int MinimumJumps(int[] forbidden, int a, int b, int x)
+    {
+        return -1;
+    }
+
+    private void Run(Jumps history)
+    {
+        
+    }
+}
+
+public class Jump
+{
+    public int Location;
+    public int Step;
+
+    public Jump Right;
+    public bool? RightAllowed;
+    
+    public Jump Left;
+    public bool? LeftAllowed;
+}
+
+public class Jumps
+{
+    public Dictionary<int, Jump> Visited = new Dictionary<int, Jump>();
+}
