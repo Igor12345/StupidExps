89002f0bb5077a0dd581edac2540f4b4d53ca182
Igor <montru@inbox.ru>
2022-11-28 00:57:54 +0100

    ShortestPathInBinaryMatrix_1091 optimized

--- SomeCoding/LC/FloodFill_733/Distance/ShortestPathInBinaryMatrix_1091.cs
+++ SomeCoding/LC/FloodFill_733/Distance/ShortestPathInBinaryMatrix_1091.cs
@@ -5,9 +5,9 @@ namespace Distance;
 public class ShortestPathInBinaryMatrix_1091
 {
     private bool _canContinue = true;
-    private Queue<(int, int)> _nextSteps = new Queue<(int, int)>();
-    private Dictionary<(int, int), List<(int, int)>> _paths = new Dictionary<(int, int), List<(int, int)>>();
+    private readonly Queue<((int, int), int)> _nextSteps = new();
     private int[][] _grid;
+    private int[][] _paths;
 
     public int ShortestPathBinaryMatrix(int[][] grid)
     {
@@ -15,46 +15,47 @@ public class ShortestPathInBinaryMatrix_1091
         if (grid[0][0] != 0 || grid[^1][^1] != 0)
             return -1;
         
-        _nextSteps.Enqueue((0, 0));
-        _paths.Add((0, 0), new List<(int, int)> { (0, 0) });
+        _paths = new int[grid.Length][];
+        for (int i = 0; i < grid.Length; i++)
+        {
+            _paths[i] = Enumerable.Repeat(-1, grid[0].Length).ToArray();
+        }
+        
+        _nextSteps.Enqueue(((0, 0), 1));
+        _paths[0][0] = 1;
         while (_nextSteps.Any())
         {
             DoStep();
         }
 
-        int pathLength = _paths.ContainsKey((_grid.Length - 1, _grid.Length - 1))
-            ? _paths[(_grid.Length - 1, _grid.Length - 1)].Count()
-            : -1;
-        return pathLength;
+        return _paths[^1][^1];
     }
 
     private void DoStep()
     {
         var point = _nextSteps.Dequeue();
-        _grid[point.Item1][point.Item2] = -1;
-        if (point.Item1 == _grid.Length - 1 && point.Item2 == _grid.Length - 1)
+        _grid[point.Item1.Item1][point.Item1.Item2] = -1;
+        if (point.Item1.Item1 == _grid.Length - 1 && point.Item1.Item2 == _grid.Length - 1)
         {
             _nextSteps.Clear();
             return;
         }
-        
-        foreach ((int, (int, int)) zero in SeeAround(point).Where(r =>r.Item1==0))
+
+        foreach ((int, (int, int)) zero in SeeAround(point.Item1).Where(r => r.Item1 == 0))
         {
-            List<(int, int)> path = new List<(int, int)>(_paths[point]) { zero.Item2 };
-            if (_paths.ContainsKey(zero.Item2))
+            if (_paths[zero.Item2.Item1][zero.Item2.Item2] != -1)
             {
-                if (_paths[zero.Item2].Count > path.Count)
-                    _paths[zero.Item2] = path;
+                if (_paths[zero.Item2.Item1][zero.Item2.Item2] > point.Item2 + 1)
+                    _paths[zero.Item2.Item1][zero.Item2.Item2] = point.Item2 + 1;
             }
             else
             {
-                _paths.Add(zero.Item2, path);
+                _paths[zero.Item2.Item1][zero.Item2.Item2] = point.Item2 + 1;
             }
-            _grid[zero.Item2.Item1][zero.Item2.Item2] = -1;
-            _nextSteps.Enqueue(zero.Item2);
-        }
 
-        _paths.Remove(point);
+            _grid[zero.Item2.Item1][zero.Item2.Item2] = -1;
+            _nextSteps.Enqueue((zero.Item2, point.Item2 + 1));
+        }
     }
 
     private IEnumerable<(int, (int, int))> SeeAround((int, int) point)
