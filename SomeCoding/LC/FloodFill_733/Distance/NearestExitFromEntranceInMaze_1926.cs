3927e3d04f40fc7d2156bea0c5f108ac905e0a62
Igor <montru@inbox.ru>
2022-11-30 23:42:34 +0100

    NearestExitFromEntranceInMaze_1926

---
+++ SomeCoding/LC/FloodFill_733/Distance/NearestExitFromEntranceInMaze_1926.cs
@@ -0,0 +1,59 @@
+﻿namespace Distance;
+
+public class NearestExitFromEntranceInMaze_1926
+{
+    private Queue<((int, int), int)> _queue = new();
+    public int NearestExit(char[][] maze, int[] entrance) {
+        int step = 1;
+        _queue.Enqueue(((entrance[0], entrance[1]), step));
+        maze[entrance[0]][entrance[1]] = '*';
+        while (_queue.Any())
+        {
+            var position = _queue.Dequeue();
+            foreach (var next in NextLocations(maze, position))
+            {
+                if (Exit(next.Item1, maze.Length, maze[0].Length))
+                {
+                    return next.Item2;
+                }
+
+                _queue.Enqueue(((next.Item1), next.Item2 + 1));
+            }
+        }
+
+        return -1;
+    }
+
+    private bool Exit((int, int) location, int lengthX, int lengthY)
+    {
+        return location.Item1 == 0 || location.Item1 == lengthX - 1 || location.Item2 == 0 || location.Item2 == lengthY - 1;
+    }
+
+    private IEnumerable<((int, int), int)> NextLocations(char[][] maze, ((int, int), int) position)
+    {
+        if (position.Item1.Item1 > 0 && maze[position.Item1.Item1 - 1][position.Item1.Item2] == '.')
+        {
+            maze[position.Item1.Item1 - 1][position.Item1.Item2] = '*';
+            yield return ((position.Item1.Item1 - 1, position.Item1.Item2), position.Item2);
+        }
+
+        if (position.Item1.Item1 < maze.Length - 1 && maze[position.Item1.Item1 + 1][position.Item1.Item2] == '.')
+        {
+            maze[position.Item1.Item1 + 1][position.Item1.Item2] = '*';
+            yield return ((position.Item1.Item1 + 1, position.Item1.Item2), position.Item2);
+        }
+
+        if (position.Item1.Item2 > 0 && maze[position.Item1.Item1][position.Item1.Item2 - 1] == '.')
+        {
+            maze[position.Item1.Item1][position.Item1.Item2 - 1] = '*';
+            yield return ((position.Item1.Item1, position.Item1.Item2 - 1), position.Item2);
+        }
+
+        if (position.Item1.Item2 < maze[0].Length - 1 && maze[position.Item1.Item1][position.Item1.Item2 + 1] == '.')
+        {
+            maze[position.Item1.Item1][position.Item1.Item2 + 1] = '*';
+            yield return ((position.Item1.Item1, position.Item1.Item2 + 1), position.Item2);
+        }
+    }
+
+}
---
+++ SomeCoding/LC/FloodFill_733/DistanceTestds/NearestExitFromEntranceInMaze_1926Tests.cs
@@ -0,0 +1,50 @@
+﻿using Distance;
+
+namespace DistanceTestds;
+
+public class NearestExitFromEntranceInMaze_1926Tests
+{
+    [Fact]
+    public void Test1()
+    {
+        char[][] maze = new[]
+        {
+            new[] { '+','+','.','+' },
+            new[] { '.','.','.','+' },
+            new[] { '+','+','+','.' }
+        };
+        var solution = new NearestExitFromEntranceInMaze_1926();
+        int steps = solution.NearestExit(maze, new[]{1,2});
+        var t = steps;
+    }
+    
+    [Fact]
+    public void Test2()
+    {
+        char[][] maze = new[]
+        {
+            new[] { '+','+','+' },
+            new[] { '.','.','.' },
+            new[] { '+','+','+' }
+        };
+        var solution = new NearestExitFromEntranceInMaze_1926();
+        int steps = solution.NearestExit(maze, new[]{1,0});
+        Assert.True(steps == 2);
+    }
+    
+    [Fact]
+    public void Test3()
+    {
+        char[][] maze = new[]
+        {
+            new[] { '.','+','+','+','+' },
+            new[] { '.','+','.','.','.' },
+            new[] { '.','+','.','+','.' },
+            new[] { '.','.','.','+','.' },
+            new[] { '+','+','+','+','.' },
+        };
+        var solution = new NearestExitFromEntranceInMaze_1926();
+        int steps = solution.NearestExit(maze, new[] { 0, 0 });
+        Assert.True(steps == 1);
+    }
+}
