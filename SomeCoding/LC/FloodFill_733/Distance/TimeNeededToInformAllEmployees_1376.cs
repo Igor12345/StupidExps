628b7e8a60d52bdaff11fe7733623bae6697df44
Igor <montru@inbox.ru>
2022-12-03 22:12:05 +0100

    TimeNeededToInformAllEmployees_1376

--- SomeCoding/LC/FloodFill_733/Distance/TimeNeededToInformAllEmployees_1376.cs
+++ SomeCoding/LC/FloodFill_733/Distance/TimeNeededToInformAllEmployees_1376.cs
@@ -1,14 +1,10 @@
-﻿using System.Collections;
-
-namespace Distance;
+﻿namespace Distance;
 
 public class TimeNeededToInformAllEmployees_1376
 {
     private Dictionary<int, Branch> _departments = new();
     public int NumOfMinutes(int n, int headID, int[] manager, int[] informTime)
     {
-
-        int ceo = -1;
         for (int i = 0; i < n; i++)
         {
             Branch employee;
@@ -22,11 +18,8 @@ public class TimeNeededToInformAllEmployees_1376
                 _departments.Add(i, employee);
             }
 
-            if (manager[i] == headID)
+            if (i != headID)
             {
-                ceo = i;
-            }
-            else{
                 if (!_departments.ContainsKey(manager[i]))
                 {
                     _departments.Add(manager[i], new Branch(manager[i], informTime[manager[i]], employee));
@@ -38,65 +31,10 @@ public class TimeNeededToInformAllEmployees_1376
             }
         }
 
-        int wholeTime = 0;
 
-        Branch header = _departments[ceo];
-        int time = header.RealTime();
-
-        TreeWalker walker = new TreeWalker(header);
-
-        IVisitor visitor = new TimeCalculator();
-        foreach (Branch branch in walker)
-        {
-            visitor.Visit(branch);
-            // Console.WriteLine($"Result of walking branch: {branch.Header}");
-        }
-        
-        Queue<int> queue = new();
-        queue.Enqueue(ceo);
-        while (queue.Any())
-        {
-            var departmentId = queue.Dequeue();
-            var department = _departments[departmentId];
-            // int time = department.Employees.m
-        }
-
-        return 1;
-    }
-
-    class TreeWalker : IEnumerable<Branch>
-    {
-        private readonly Branch _branch;
-
-        public TreeWalker(Branch branch)
-        {
-            // Console.WriteLine($"ctor walker {branch.Header} [{branch.Employees.Count}]");
-            _branch = branch;
-        }
-
-        public IEnumerator<Branch> GetEnumerator()
-        {
-            // Console.WriteLine($"Enter GetEnumerator of {_branch.Header}");
-            foreach (Branch employee in _branch.Employees)
-            {
-                // Console.WriteLine($"Enter loop from {_branch.Header} - employee: {employee.Header}");
-                foreach (Branch sub in new TreeWalker(employee))
-                {
-                    // Console.WriteLine($"Return {sub.Header}; from {_branch.Header} - employee: {employee.Header}");
-                    yield return sub;
-                }
-
-                // Console.WriteLine($"Return employee himself {employee.Header} from {_branch.Header}");
-                // yield return employee;
-            }
-
-            yield return _branch;
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
+        Branch header = _departments[headID];
+        int wholeTime = header.RealTime();
+        return wholeTime;
     }
 
     struct Branch
@@ -119,37 +57,10 @@ public class TimeNeededToInformAllEmployees_1376
             Employees.Add(employee);
         }
 
-        public void Accept(IVisitor visitor)
-        {
-            visitor.Visit(this);
-        }
-
         public int RealTime()
         {
             int result = Time + (Employees.Any() ? Employees.Max(e => e.RealTime()) : 0);
             return result;
-        }
-    }
-
-    interface IVisitor
-    {
-        void Visit(Branch branch);
-    }
-
-    class PrintVisitor : IVisitor
-    {
-        public void Visit(Branch branch)
-        {
-            Console.WriteLine($"!! Processing {branch.Header} [{branch.Employees.Count}]");
-        }
-    }
-
-    class TimeCalculator : IVisitor
-    {
-        public void Visit(Branch branch)
-        {
-            Console.WriteLine($"!! Processing {branch.Header} [{branch.Employees.Count}], time: {branch.RealTime()}");
-
         }
     }
 }
--- SomeCoding/LC/FloodFill_733/DistanceTestds/TimeNeededToInformAllEmployees_1376Tests.cs
+++ SomeCoding/LC/FloodFill_733/DistanceTestds/TimeNeededToInformAllEmployees_1376Tests.cs
@@ -11,9 +11,31 @@ public class TimeNeededToInformAllEmployees_1376Tests
         int[] informTime = new[] { 0, 4, 0, 1, 0, 3, 2, 0 };
 
         var solution = new TimeNeededToInformAllEmployees_1376();
-        var time = solution.NumOfMinutes(8, -1, manager, informTime);
+        var time = solution.NumOfMinutes(8, 5, manager, informTime);
         
         var t = time;
 
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        int[] manager = new[] { -1 };
+        int[] informTime = new[] { 0 };
+
+        var solution = new TimeNeededToInformAllEmployees_1376();
+        var time = solution.NumOfMinutes(1, 0, manager, informTime);
+        
+        var t = time;
+    }[Fact]
+    public void Test2()
+    {
+        int[] manager = new[] { 2,2,-1,2,2,2 };
+        int[] informTime = new[] { 0,0,1,0,0,0 };
+
+        var solution = new TimeNeededToInformAllEmployees_1376();
+        var time = solution.NumOfMinutes(6, 2, manager, informTime);
+        
+        var t = time;
     }
 }
