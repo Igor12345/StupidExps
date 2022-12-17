284c4e0dfc6876a667b671b3f667871b8ab42785
Igor <montru@inbox.ru>
2022-12-17 21:29:45 +0100

    MaximalNetworkRank1615

---
+++ SomeCoding/LC/FloodFill_733/Connections/MaximalNetworkRank1615.cs
@@ -0,0 +1,51 @@
+﻿namespace Connections;
+
+public class MaximalNetworkRank1615
+{
+    public int MaximalNetworkRank(int n, int[][] roads)
+    {
+        for (int i = 0; i < roads.Length; i++)
+        {
+            int first = roads[i][0];
+            int second = roads[i][1];
+            (int, int) key = (Math.Min(first, second), Math.Max(first, second));
+            if (!_pairs.Contains(key))
+            {
+                _pairs.Add(key);
+                AddOrIncreaseAdjacency(first);
+                AddOrIncreaseAdjacency(second);
+            }
+        }
+        
+        int max = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (!_adjacency.ContainsKey(i)) continue;
+            for (int j = i + 1; j < n; j++)
+            {
+                if (!_adjacency.ContainsKey(j)) continue;
+                bool decrease = _pairs.Contains((i, j));
+                int pairRank = _adjacency[i] + _adjacency[j] - (decrease ? 1 : 0);
+                if (pairRank > max)
+                    max = pairRank;
+            }
+        }
+        return max;
+    }
+
+    private void AddOrIncreaseAdjacency(int vertex)
+    {
+        if (!_adjacency.ContainsKey(vertex))
+        {
+            _adjacency.Add(vertex, 1);
+        }
+        else
+        {
+            _adjacency[vertex] += 1;
+        }
+    }
+
+    private readonly HashSet<(int, int)> _pairs = new HashSet<(int, int)>();
+    private Dictionary<int, int> _adjacency = new Dictionary<int, int>();
+
+}
---
+++ SomeCoding/LC/FloodFill_733/ConnectionsTests/MaximalNetworkRank1615Tests.cs
@@ -0,0 +1,72 @@
+﻿using Connections;
+
+namespace ConnectionsTests;
+
+public class MaximalNetworkRank1615Tests
+{
+    [Fact]
+    public void Test1()
+    {
+        int n = 4;
+        int[][] roads = new[]
+        {
+            new[] { 0, 1 },
+            new[] { 0, 3 },
+            new[] { 1, 2 },
+            new[] { 1, 3 }
+        };
+        MaximalNetworkRank1615 solution = new MaximalNetworkRank1615();
+        int result = solution.MaximalNetworkRank(n, roads);
+
+        var t = result;
+    }
+    
+    [Fact]
+    public void Test2()
+    {
+        int n = 5;
+        int[][] roads = new[]
+        {
+            new[] { 0, 1 },
+            new[] { 0, 3 },
+            new[] { 1, 2 },
+            new[] { 1, 3 },
+            new[] { 2, 3 },
+            new[] { 2, 4 }
+        };
+        MaximalNetworkRank1615 solution = new MaximalNetworkRank1615();
+        int result = solution.MaximalNetworkRank(n, roads);
+
+        var t = result;
+    }
+    
+    [Fact]
+    public void Test3()
+    {
+        int n = 8;
+        int[][] roads = new[]
+        {
+            new[] { 0, 1 },
+            new[] { 1, 2 },
+            new[] { 2, 3 },
+            new[] { 2, 4 },
+            new[] { 5, 6 },
+            new[] { 5, 7 }
+        };
+        MaximalNetworkRank1615 solution = new MaximalNetworkRank1615();
+        int result = solution.MaximalNetworkRank(n, roads);
+
+        var t = result;
+    }
+    
+    [Fact]
+    public void Test4()
+    {
+        int n = 2;
+        int[][] roads = Array.Empty<int[]>();
+        MaximalNetworkRank1615 solution = new MaximalNetworkRank1615();
+        int result = solution.MaximalNetworkRank(n, roads);
+
+        var t = result;
+    }
+}
