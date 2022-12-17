50a1e342d9141d0a5d866149e8edbb1529b68dd6
Igor <montru@inbox.ru>
2022-12-17 21:32:59 +0100

    SafeFireAndForgetExtensions program

---
+++ SomeCoding/Learning/Learning/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+var t = new SeveralTasks();
+
+Console.WriteLine("Start");
+
+await t.JustRun();
+ // t.JustRun().SafeFireAndForget(e=>{
+ //     Console.WriteLine($"Handler {e}");
+ //     throw e;
+ // });
+
+await Task.Delay(20);
+ Console.WriteLine("End");
+ 
+ Console.ReadLine();
