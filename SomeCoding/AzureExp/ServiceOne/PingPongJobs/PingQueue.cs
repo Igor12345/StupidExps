c7b78d5629a20bc68421d08915ff33dcafe6be47
Igor <montru@inbox.ru>
2022-12-22 22:02:53 +0100

    Create first web job project

---
+++ SomeCoding/AzureExp/ServiceOne/PingPongJobs/PingQueue.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPongJobs
+{
+   public class PingQueue
+   {
+      public static void ProcessQueueMessage([QueueTrigger("cool-queue")] string message, ILogger logger)
+      {
+         logger.LogWarning(message);
+      }
+   }
+}
---
+++ SomeCoding/AzureExp/ServiceOne/PingPongJobs/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace PingPongJobs
+{
+   class Program
+   {
+      static async Task Main()
+      {
+         var builder = new HostBuilder();
+         builder.UseEnvironment(EnvironmentName.Development);
+         builder.ConfigureWebJobs(b =>
+         {
+            b.AddAzureStorageCoreServices();
+            b.AddAzureStorageQueues();
+         });
+         builder.ConfigureLogging((context, b) =>
+         {
+            b.AddConsole();
+            b.SetMinimumLevel(LogLevel.Warning);
+            string instrumentationKey = context.Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"];
+            if (!string.IsNullOrEmpty(instrumentationKey))
+            {
+               b.AddApplicationInsightsWebJobs(o => o.InstrumentationKey = instrumentationKey);
+            }
+         });
+         var host = builder.Build();
+         using (host)
+         {
+            await host.RunAsync();
+         }
+      }
+   }
+}
